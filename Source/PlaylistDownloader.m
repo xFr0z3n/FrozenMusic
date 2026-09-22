2724758a3f1c180b9b80b39eebb25429c1cb4e32
xFr0z3n <114761664+xFr0z3n@users.noreply.github.com>
2026-09-22 16:17:38 +0200

    Update PlaylistDownloader.m

--- Source/PlaylistDownloader.m
+++ Source/PlaylistDownloader.m
@@ -360,6 +360,7 @@ static BOOL YTMUPatchMP3TrackNumber(NSURL *fileURL, NSInteger position) {
 @property (nonatomic, strong) NSTimer *watchdog;
 @property (nonatomic, copy) NSString *collectionCoverURL; // playlist / album cover for cover.png
 @property (nonatomic, copy) NSString *firstTrackCoverURL; // album fallback
+@property (nonatomic, strong) NSDictionary *lastBrowseResponse; // for _debug_title.json
 @property (nonatomic, strong) FFMpegDownloader *coverWriter;
 @end
 
@@ -499,6 +500,7 @@ static BOOL YTMUPatchMP3TrackNumber(NSURL *fileURL, NSInteger position) {
 
     self.running = YES;
     self.cancelled = NO;
+    self.lastBrowseResponse = nil;
     self.isAlbum = [browseID hasPrefix:@"MPREb_"] || [browseID hasPrefix:@"VLOLAK5uy_"];
     self.albumArtist = nil;
     self.albumYear = nil;
@@ -528,8 +530,13 @@ static BOOL YTMUPatchMP3TrackNumber(NSURL *fileURL, NSInteger position) {
                 [self finishWithMessage:LOC(@"OOPS") details:@"Couldn't load the songs (private playlists aren't supported yet)" icon:@"xmark"];
                 return;
             }
-            self.titleIsGuess = title.length == 0;
-            [self confirmDownloadOfTracks:tracks title:title.length ? title : (self.isAlbum ? @"Album" : @"Playlist")];
+            NSString *finalTitle = title;
+            if (!finalTitle.length) {
+                finalTitle = [self titleFromPageTextsExcluding:tracks];
+                [self writeTitleDebugForBrowseID:browseID pickedTitle:finalTitle];
+            }
+            self.titleIsGuess = finalTitle.length == 0;
+            [self confirmDownloadOfTracks:tracks title:finalTitle.length ? finalTitle : (self.isAlbum ? @"Album" : @"Playlist")];
         });
     });
 }
@@ -538,6 +545,75 @@ static BOOL YTMUPatchMP3TrackNumber(NSURL *fileURL, NSInteger position) {
     [self endKeepAlive];
     self.running = NO;
     [self showMessage:title details:details icon:icon];
+}
+
+#pragma mark Title fallback from the page
+
+- (NSString *)titleFromPageTextsExcluding:(NSArray<YTMUPlaylistTrack *> *)tracks {
+    NSMutableSet<NSString *> *exclude = [NSMutableSet set];
+    for (YTMUPlaylistTrack *track in tracks) {
+        if (track.title)
+            [exclude addObject:track.title.lowercaseString];
+        if (track.artist)
+            [exclude addObject:track.artist.lowercaseString];
+    }
+    NSArray *noise = @[@"download", @"play", @"shuffle", @"save", @"more", @"comments", @"share", @"edit", @"back", @"search", @"add a song", @"library", @"home", @"samples", @"downloads"];
+
+    // 1. Something marked as a heading
+    for (NSDictionary *text in self.pageTexts) {
+        NSString *label = text[@"label"];
+        if ([text[@"header"] boolValue] && ![text[@"button"] boolValue] && ![exclude containsObject:label.lowercaseString])
+            return label;
+    }
+    // 2. First plain text that isn't a button, a stats line or a song on the page
+    for (NSDictionary *text in self.pageTexts) {
+        NSString *label = text[@"label"];
+        NSString *lower = label.lowercaseString;
+        if ([text[@"button"] boolValue] || label.length < 2 || label.length > 120)
+            continue;
+        if ([label containsString:@"•"] || [label containsString:@"\n"] || [exclude containsObject:lower])
+            continue;
+        BOOL isNoise = NO;
+        for (NSString *word in noise) {
+            if ([lower isEqualToString:word] || [lower hasPrefix:[word stringByAppendingString:@" "]])
+                isNoise = YES;
+        }
+        if (!isNoise && self.albumArtist.length && [lower isEqualToString:self.albumArtist.lowercaseString])
+            isNoise = YES;
+        if (!isNoise)
+            return label;
+    }
+    return nil;
+}
+
+// Written only when the page title couldn't be read from YouTube's data
+- (void)writeTitleDebugForBrowseID:(NSString *)browseID pickedTitle:(NSString *)picked {
+    NSMutableDictionary *debug = [NSMutableDictionary dictionary];
+    debug[@"browseID"] = browseID ?: @"?";
+    debug[@"isAlbum"] = @(self.isAlbum);
+    debug[@"pickedFromPage"] = picked ?: @"(none)";
+    debug[@"pageTexts"] = self.pageTexts ?: @[];
+    debug[@"nowPlaying"] = [self jsonSafe:YTMUCurrentNowPlayingInfo()] ?: @{};
+    debug[@"response"] = self.lastBrowseResponse ?: @{};
+
+    NSURL *documents = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    NSURL *folder = [documents URLByAppendingPathComponent:@"YTMusicUltimate"];
+    [[NSFileManager defaultManager] createDirectoryAtURL:folder withIntermediateDirectories:YES attributes:nil error:nil];
+    NSData *data = [NSJSONSerialization dataWithJSONObject:debug options:NSJSONWritingPrettyPrinted error:nil];
+    [data writeToURL:[folder URLByAppendingPathComponent:@"_debug_title.json"] atomically:YES];
+}
+
+// Lock-screen info contains non-JSON objects (artwork): keep strings/numbers only
+- (NSDictionary *)jsonSafe:(NSDictionary *)info {
+    NSMutableDictionary *safe = [NSMutableDictionary dictionary];
+    for (id key in info) {
+        id value = info[key];
+        if ([value isKindOfClass:[NSString class]] || [value isKindOfClass:[NSNumber class]])
+            safe[[key description]] = value;
+        else
+            safe[[key description]] = NSStringFromClass([value class]);
+    }
+    return safe;
 }
 
 #pragma mark Playlist fetching (InnerTube web API)
@@ -562,6 +638,8 @@ static BOOL YTMUPatchMP3TrackNumber(NSURL *fileURL, NSInteger position) {
     NSDictionary *response = YTMUPostJSON(baseURL, @{@"context": [self webContext], @"browseId": browseID}, [self webHeaders]);
     if (!response)
         return tracks;
+    if (!self.lastBrowseResponse)
+        self.lastBrowseResponse = response;
 
     // Title (+ artist and year for albums) from the page header
     for (NSString *headerKey in @[@"musicResponsiveHeaderRenderer", @"musicDetailHeaderRenderer", @"musicEditablePlaylistDetailHeaderRenderer", @"musicImmersiveHeaderRenderer", @"musicVisualHeaderRenderer"]) {
